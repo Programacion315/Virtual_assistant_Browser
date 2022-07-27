80670c67762f4992a5842a7872e2d49e68607de4
jluiso315 <jorgeluishuergo88@gmail.com>
2022-07-27 16:10:25 -0500

    fix: the position of the secondary screen

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,5 +1,4 @@
 from random import randint
-import time
 from tkinter import *
 import ctypes
 from urllib import response
@@ -9,6 +8,10 @@ root = Tk()
 #root.overrideredirect(True) #Le quita el titulo, poner al final
 root.wm_attributes('-transparentcolor', '#F0F0F0')
 
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+ancho, alto = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
 
 def getJoke():
     joke = requests.get(f"https://icanhazdadjoke.com/", headers= {"Accept":"application/json"})
@@ -16,19 +19,17 @@ def getJoke():
     print(joke['joke'])
     ventana_secundaria = Toplevel()
     ventana_secundaria.title("Message")
-    loc = "900x200+1+1"
-    ventana_secundaria.geometry(loc)
 
-    
+    halfWidth = root.winfo_screenwidth() // 2 - 900 // 2
+    halfHigh = root.winfo_screenheight() // 2 - 200 // 2
+
+    loc = "900x200+" + str(halfWidth) + "+" + str(halfHigh)
+    ventana_secundaria.geometry(loc)
     Label(ventana_secundaria, text=joke['joke'],
-             font=("Agency FB", 14)).place(x=50, y=50) 
-    
+             font=("Agency FB", 14)).place(x=5, y=100),
 
 
 def position():
-    user32 = ctypes.windll.user32
-    user32.SetProcessDPIAware()
-    ancho, alto = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
     restaAncho = int(ancho * 0.20)
     restaAlto = int(alto * 0.40)
     x, y = str(ancho - restaAncho), str(alto - restaAlto)
